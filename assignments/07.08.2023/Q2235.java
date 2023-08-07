636747a66fc33254a81ec292ec06a0f3157be0b1
sm1444 <124179352+sm1444@users.noreply.github.com>
2023-08-07 14:20:41 +0530

    new commit

---
+++ assignments/07.08.2023/Q2235.java
@@ -0,0 +1,8 @@
+//2235. Add Two Integers
+
+class Q2235 {
+    public static int sum(int num1, int num2) {
+        int ans = num1+num2;
+        return ans;
+    }
+}
---
+++ assignments/07.08.2023/Q2413.java
@@ -0,0 +1,14 @@
+//2413. Smallest Even Multiple
+
+class Q2413 {
+    public int smallestEvenMultiple(int n) {
+        if(n%2==0)
+        {
+            return n;
+        }
+        else{
+            return n*2;
+        }
+    }
+}
+
