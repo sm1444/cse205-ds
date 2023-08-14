31e85bb5d8b55f75b40bf60e5854a1550590fa4f
sm1444 <124179352+sm1444@users.noreply.github.com>
2023-08-14 14:13:14 +0530

    third commit

---
+++ assignments/14.08.2023/P231.java
@@ -0,0 +1,19 @@
+public class P231 {
+    public boolean isPowerOfTwo(int n) {
+        if (n==1)
+        {
+            return true;
+        } 
+        else if(n==0)
+        {
+            return false;
+        }
+        else if(n%2 !=0)
+        {
+            return false;
+        }
+        else{
+            return isPowerOfTwo(n/2);
+        }
+     }
+}
